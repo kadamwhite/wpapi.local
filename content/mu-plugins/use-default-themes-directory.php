810c852ccb41c1200275f108559233f1ed8bf4af
K. Adam White <kadamwhite@users.noreply.github.com>
2015-12-30 11:16:31 -0500

    Add MU plugin to ensure default themes are available
    We use a custom content directory but still want to access the default
    WordPress themes. This lets us do so.

---
+++ content/mu-plugins/use-default-themes-directory.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Access Default Themes
+Description: Permit access to the themes in the built-in wp-content/themes directory even when a custom content directory is used
+Version:     1.0
+Author:      K. Adam White
+Author URI:  https://wordpress.org/support/profile/kadamwhite
+ */
+register_theme_directory( ABSPATH . '/wp-content/themes' );
